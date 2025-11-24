8fc2dfdde526c44e379af63aa780c72c5876260d
Mossyr <moacircadmiel51@gmail.com>
2025-11-24 19:15:24 -0300

    Update publish.js

--- assets/js/publish.js
+++ assets/js/publish.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://fiscally-untasting-janel.ngrok-free.dev -> http://localhost:3000/api';
+const API_BASE_URL = 'https://fiscally-untasting-janel.ngrok-free.dev/api';
 
 document.getElementById('publish-form').addEventListener('submit', async function(event) {
     event.preventDefault();
@@ -66,3 +66,4 @@ document.getElementById('publish-form').addEventListener('submit', async functio
         console.error('Erro na requisição de upload:', error);
     }
 });
+
